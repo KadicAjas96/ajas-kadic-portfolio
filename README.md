7148d8ab05fcd9f556b5ea24cf94323cf9ccf710
KadicAjas96 <ajas.kadic@seattlecolleges.edu>
2019-12-03 20:12:46 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# portfolio-2018-1wx7vh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/portfolio-2018-1wx7vh)
---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing-module';
+
+import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidenavComponent,
+    AboutComponent,
+    ResumeComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
